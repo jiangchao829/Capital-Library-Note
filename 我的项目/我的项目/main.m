f1e277a48d703ebee10bbd454f53614512627ce6
jiangchao829 <1039470758@qq.com>
2016-11-29 20:36:25 +0800

    初始化项目

---
+++ 我的项目/我的项目/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  我的项目
+//
+//  Created by 姜超 on 16/11/29.
+//  Copyright © 2016年  JiangChao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
